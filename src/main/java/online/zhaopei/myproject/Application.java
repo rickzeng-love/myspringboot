26e0d8b1a261fe21116e42a5c2e4d8f002b7353d
zengtao <rickzeng.love@gmail.com>
2018-04-26 22:26:40 +0800

    springboot

---
+++ src/main/java/online/zhaopei/myproject/Application.java
@@ -0,0 +1,21 @@
+package online.zhaopei.myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+//@ImportResource({"classpath:config/spring-integration-file-context.xml"})
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
+	}
+	
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
+}
